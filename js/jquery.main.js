526a6e64848d39a787849c1a01beed0b9f783ad8
Максим <max.tkh.ua@gmail.com>
2016-01-28 11:13:24 +0200

    projects

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -447,3 +447,112 @@ Gallery.prototype = {
         };
     }
 };
+
+var Popup = function( obj ){
+    this.popup = obj;
+    this.btnShow = $('.popup__open');
+    this.btnClose = obj.find( '.popup__close, .popup__cancel' );
+    this.wrap = obj.find($('.popup__wrap'));
+    this.contents = obj.find($('.popup__content'));
+    this.window = $( window );
+    this.scrollConteiner = $( 'html' );
+    this.timer = setTimeout( function(){},1 );
+
+    this.init();
+};
+Popup.prototype = {
+    init: function(){
+        var self = this;
+        self.core = self.core();
+        self.core.build();
+    },
+    core: function (){
+        var self = this;
+        return {
+            build: function (){
+                self.core.controls();
+            },
+            controls: function(){
+                $('body').on( 'click','.popup__open', function(){
+                    var curItem = $(this),
+                        parentDropdown = curItem.parents(".dropdown"),
+                        linkDropdown = parentDropdown.find("a[data-toggle=dropdown]");
+                    parentDropdown.removeClass("open");
+                    linkDropdown.attr("aria-expanded", "false");
+                    self.core.show( curItem.attr( 'data-popup' ) );
+                    popup.btnClose = self.popup.find(".popup__close");
+                    $('.popup_opened').find('#order-popup__type').val( curItem.attr( 'data-type' ) );
+                    return false;
+                } );
+
+                self.wrap.on( {
+                    click: function( event ){
+                        event = event || window.event;
+
+                        if (event.stopPropagation) {
+                            event.stopPropagation();
+                        } else {
+                            event.cancelBubble = true;
+                        }
+                    }
+                } );
+                self.popup.on( {
+                    click: function(){
+                        self.core.hide();
+                        return false;
+                    }
+                } );
+                self.btnClose.on( {
+                    click: function(){
+                        self.core.hide();
+                        return false;
+                    }
+                } );
+            },
+            hide: function(){
+                self.popup.css ({
+                    'overflow-y': "hidden"
+                });
+                self.scrollConteiner.css({
+                    paddingRight: 0,
+                    'overflow-y': "scroll"
+                });
+                self.popup.removeClass('popup_opened');
+                self.popup.addClass('popup_hide');
+                location.hash = '';
+                setTimeout( function(){
+                    self.popup.removeClass('popup_hide');
+                }, 300 );
+            },
+            getScrollWidth: function (){
+                var scrollDiv = document.createElement("div");
+                scrollDiv.className = "popup__scrollbar-measure";
+                document.body.appendChild(scrollDiv);
+                var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+                document.body.removeChild(scrollDiv);
+                return scrollbarWidth;
+            },
+            show: function( className ){
+                if (self.contents.height()+120 > self.window.height()){
+                    self.popup.css ({
+                        'overflow-y': "scroll"
+                    });
+                    self.scrollConteiner.css( {
+                        'overflow-y': "hidden",
+                        paddingRight: 17
+                    });
+                }
+                self.core.setPopupContent( className );
+                self.popup.addClass('popup_opened');
+                $('.popup_opened').find('#order-popup__name').focus();
+            },
+            setPopupContent: function( className ){
+                self.contents = self.popup.find('.popup__content');
+                var curContent = self.contents.filter( '.popup__' + className );
+                self.contents.css( { display: 'none' } );
+                curContent.css( { display: 'block' } );
+            }
+
+        };
+    }
+};
